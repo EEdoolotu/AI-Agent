3a6c4df655f4a747ef79e10bee7a1742136abc2e
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-11-08 13:55:29 +0000

    python

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    absolute_full_path = os.path.abspath(full_path)
+    absolute_working_path = os.path.abspath(working_directory)
+
+    if not absolute_full_path.startswith(absolute_working_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(absolute_full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    else:
+        try:
+            MAX_CHARS = 10000
+            with open(absolute_full_path, "r", encoding="utf-8") as f:
+                text = f.read(MAX_CHARS + 1)
+                count = len(text)
+                if count <= MAX_CHARS:
+                    return text
+                else:
+                   return text[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+
+                
+
+        except Exception as e:
+            return f"Error: {str(e)}"
+    
---
+++ functions/write_file.py
@@ -0,0 +1,26 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    full_path = os.path.join(working_directory, file_path)
+    absolute_full_path = os.path.abspath(full_path)
+    absolute_working_path = os.path.abspath(working_directory)
+
+    try:
+        if not absolute_full_path.startswith(absolute_working_path):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        elif not os.path.exists(absolute_full_path):
+            dir_path = os.path.dirname(absolute_full_path)
+            os.makedirs(dir_path, exist_ok=True)
+            with open(absolute_full_path, "w") as f:
+                f.write(content)
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        elif os.path.isfile(absolute_full_path):
+            with open(absolute_full_path, "w") as f:
+                f.write(content)
+                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        else:
+            return f"Error: {file_path} is a directory not a file"
+    except Exception as e:
+            return f"Error: {str(e)}"
+
+
